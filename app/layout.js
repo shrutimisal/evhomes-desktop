9b7f5d914a6339bf997fcc5d88b56714e644d0e2
shruti misal <shrutimisal109@gmail.com>
2025-03-29 11:22:36 +0530

    layout chnages

--- app/layout.js
+++ app/layout.js
@@ -1,47 +1,18 @@
-import Calendar from "./components/Calendar";
-import LeadsCard from "./components/LeadsCard";
-import Navbar from "./components/NavBar";
-import PieCard from "./components/PieCard";
-import Sidebar from "./components/SideBar";
-import StatsCard from "./components/StatsCard";
-import TaskCard from "./components/TaskCard";
-import TeamCard from "./components/TeamCard";
+
+import Sidebar from "./components/SideBar/SideBar";
 import "./globals.css";
 
-export const metadata = {
-  title: "Dashboard",
-  description: "User Profile Dashboard",
-};
-
-export default function RootLayout({ children }) {
+export default function Layout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body>
-        <div className="layout">
-          <div className="sidebar">
-            <Sidebar />
-          </div>
-          <div className="mainContent">
-            <Navbar />
+    <div className="layout">
+      <div className="sidebar">
+        <Sidebar />
+      </div>
 
-            <div className="statsCalendarContainer">
-              <StatsCard />
-              <Calendar />
-              <LeadsCard />
-            </div>
-            <div className="pieContainer">
-              <PieCard />
-              <TaskCard/>
-              <TeamCard/>
-            </div>
-            <div className="content">{children}</div>
-          </div>
-        </div>
-      </body>
-    </html>
+      <div className="mainContent">
+        <div className="content">{children}</div>
+      </div>
+    </div>
   );
 }
+
